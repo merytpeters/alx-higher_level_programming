72a2bf207cc5ddd1ced0d6d389ff02afaaa22e37
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-11 22:29:34 +0100

    Task 10 Linked list cycle, not complete

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ *check_cycle - Checks if a singly linked list has a cycle in it
+ *list: Pointer to a singly linked list
+ *Return: 0 if no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+
