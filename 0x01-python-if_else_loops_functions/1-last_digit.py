75ddc6f68f258b703fb9f4f33d8abce4618e240d
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-12 15:22:40 +0100

    Modified to check for negative and 0 last digits

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,21 +2,16 @@
 import random
 number = random.randint(-10000, 10000)
 str = "Last digit of"
-last_digit = int(number % 10) #variable that stores the last digit
+last_digit = abs(number) % 10  #variable that stores the last digit
 if number < 0:
-    last_digit *= int(-1)
-    if last_digit < 0:
-        print(f"{str} {number:d} is {last_digit:d} and is less than 6 and not 0")
-    elif last_digit == 0:
-            print(f"{str} {number:d} is {last_digit:d} and is 0")
-elif number > 0:
-    if last_digit > 5:
-        print(f"{str} {number:d} is {last_digit:d} and is greater than 5")
-    elif last_digit < 6:
-        print(f"{str} {number:d} is {last_digit:d} and is less than 6 and not 0")
-    elif last_digit == 0:
-        print(f"{str} {number:d} is {last_digit:d} and is 0")
+    last_digit = -(last_digit)
+if last_digit > 5:
+    print(f"{str} {number:d} is {last_digit:d} and is greater than 5")
+elif last_digit == 0:
+    print(f"{str} {number:d} is {last_digit:d} and is 0")
+elif last_digit < 6:
+    print(f"{str} {number:d} is {last_digit:d} and is less than 6 and not 0")
 elif number == 0:
-    printf(f"{str} {number:d} is {last_digit:d} and is 0")
+    printf(f"The only digit of {number:d} is {last_digit:d}")
 else:
     print("Error! not a digit")
