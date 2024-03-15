aa93306acc8ed843a7d2e308d0d284855b0309aa
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-15 04:04:01 +0100

    Argument module created

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    j = len(sys.argv) -1
+
+    if j == 0:
+        print("{} arguments." .format(j))
+    elif j == 1:
+        print("{} arguments:" .format(j))
+    else:
+        print("{} arguments:" .format(j))
+
+    if j >= 1:
+        j = 0
+        for arg in sys.argv:
+            if j !=0:
+                print("{}: {}".format(j, arg))
+            j += 1
