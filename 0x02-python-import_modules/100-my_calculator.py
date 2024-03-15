1c70ae973bdb2f79e8f8ec565723ca4f58125b39
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-15 05:16:30 +0100

    Calculator, made some corrections

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     nargs = len(sys.argv) - 1
     if nargs != 3:
-        print("Usage: ./100-my_calculator.py <a> <operatoer> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     op = sys.argv[2]
