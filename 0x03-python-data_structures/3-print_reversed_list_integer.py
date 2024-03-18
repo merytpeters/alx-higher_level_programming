be1b61f5ec97b798a61ecac0419bad437a02f80f
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-18 21:10:28 +0100

    Reversed list function, added an if condition

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     my_list = my_list[::-1]
     for num in range(len(my_list)):
-        print("{:d}" .format(my_list[num]))
+        if num < len(my_list):
+            print("{:d}" .format(my_list[num]))
