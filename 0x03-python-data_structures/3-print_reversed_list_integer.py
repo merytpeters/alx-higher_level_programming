bd9a4e3e6fe92ebaf37c1627b4df87c867fe2e80
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-18 21:00:00 +0100

    Reversed list function

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    reversedList = my_list[::-1]
-    for num in range(len(reversedList)):
-        print("{:d}" .format(reversedList[num]))
+    my_list = my_list[::-1]
+    for num in range(len(my_list)):
+        print("{:d}" .format(my_list[num]))
