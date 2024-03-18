3116d1a09cd7ebd376ed9e278cd8de6d160cf472
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-18 23:36:55 +0100

    Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][col]), end='')
+            if col != (len(matrix[row]) - 1):
+                print(" ", end='')
+        print()
