0d961f4ac86a4d9bb51143b20d256d103ea7fdf2
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-18 21:20:42 +0100

    Reversed list function, added an if condition to check if its a list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     my_list = my_list[::-1]
     for num in range(len(my_list)):
-        if num < len(my_list):
+        if isinstance(my_list, list):
             print("{:d}" .format(my_list[num]))
