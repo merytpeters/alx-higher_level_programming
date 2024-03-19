d06117660bb553ff4429702f17fe2d1606bd3659
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-19 02:03:14 +0100

    Palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+
+/**
+ *is_palindrome - Checks if a singly linked list is a palindrome
+ *@head: Pointer to the list
+ *Return: 0 if it is not a palindrome, 1 if it is
+ */
+int is_palindrome(listint_t **head)
