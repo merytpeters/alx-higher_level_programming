b279c6aee0947b6e61dddce307535a8998f5c6ee
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-19 01:02:42 +0100

    Tuple and length of tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,6 @@ def multiple_returns(sentence):
     lenSent = len(sentence)
 
     if lenSent == 0:
-        sentence[0] = None
-        return (sentence[0])
+        return (None)
     else:
         return (lenSent, sentence[0])
