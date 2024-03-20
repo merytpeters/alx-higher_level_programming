8c10cb6c2fabe476e06354d050e734f089df807a
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-20 22:17:59 +0100

    Function that computes the square value of all integers of a matrix without using regular loops, map; changed to if and for statements to meet the requirements

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result = [[row[i] ** 2 for row in matrix] for i in range (3)]
-    return (result)
+    if matrix == []:
+         return []
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append([i**2 for i in row])
+    return (new_matrix)
