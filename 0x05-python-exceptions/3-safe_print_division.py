a798f80efef1846e9068d9db55d0b4f59bbd0773
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-04-11 13:41:40 +0100

    Division function that includes ZeroDivisionError, removed other errors and added floatingPointError

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     quotient = None
     try:
         quotient = a / b
-    except (ValueError, TypeError, ZeroDivisionError):
+    except (FloatingPointError, ZeroDivisionError):
         if (a or b == 0) and (a == 0 and b == 0):
             quotient = None
         elif (a < 0 or b < 0):
