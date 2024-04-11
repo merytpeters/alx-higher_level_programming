f41d1b701de403c325cbb2d5766823059a30ba4d
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-04-11 13:10:40 +0100

    Division function that includes ZeroDivisionError, edge cases for negative numbers added , edge cases for single and double zero numbers added

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    quotient = 0
     try:
         quotient = a / b
     except (ValueError, TypeError, ZeroDivisionError):
-        quotient = None
-        pass
+        if (a or b) == 0 and (a and b) == 0:
+            quotient = None
+            pass
     finally:
         print("Inside result : {}".format(quotient), end="")
+        if (a or b) < 0:
+            quotient = quotient * -1
     print()
     return quotient
