2ead89f444bf522330c100947a4699cf8795d113
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-04-11 13:55:04 +0100

    Alx checker erroe , one frustrating extra space

--- 0x05-python-exceptions/3-main.py
+++ 0x05-python-exceptions/3-main.py
@@ -2,11 +2,11 @@
 safe_print_division = __import__('3-safe_print_division').safe_print_division
 
 a = -12
-b = -2
+b = 2
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
 
-a = 0
-b = 12
+a = 12
+b = 0
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except (FloatingPointError, ZeroDivisionError):
         quotient = None
     finally:
-        print("Inside result : {}".format(quotient))
+        print("Inside result: {}".format(quotient))
     return quotient
