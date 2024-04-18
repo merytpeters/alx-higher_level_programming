39947568c9023450360505ac225a56539c8b518a
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-04-18 16:06:48 +0100

    Square with size as integer

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """This class represents a square."""
+
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0 :
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
