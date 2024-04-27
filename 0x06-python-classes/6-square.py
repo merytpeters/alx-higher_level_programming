b466453112de67ab0747e5f7679d12ea97b0c187
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-04-27 11:10:44 +0100

    Completed, position attribute set, conditions added

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,8 @@
 
 class Square:
     """This class represents a square."""
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
+        self.__position = position
         if isinstance(size, int):
             if (size >= 0):
                 self.__size = size
@@ -27,12 +28,26 @@ class Square:
         else:
             raise TypeError("size must be an integer")
 
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def postion(self, value):
+        if isinstance(value, tuple) or len(value) == 2:
+            self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+
     def area(self):
         return self.__size ** 2
 
     def my_print(self):
         if (self.__size > 0):
             for i in range(self.__size):
-                print('#' * self.__size)
+                print(" " * self.position[0] + '#' * self.__size)
+            for i in range(self.position[1]):
+                print()
         elif (self.__size == 0):
             print()
