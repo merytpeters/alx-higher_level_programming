71299dad923f13e874d1bffbd4aeeb8ab59975d6
merytpeters <merytpeters@gmail.com>
2024-05-14 15:15:43 +0100

    A class Rectangle with private attributes height and width created, used not isinstance

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,9 +22,9 @@ class Rectangle:
     def width(self, value):
         """Sets the width"""
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
