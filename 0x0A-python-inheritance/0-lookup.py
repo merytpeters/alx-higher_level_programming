8644382a63d7ca9a7e80ef57ee1ba186a46c97fa
merytpeters <merytpeters@gmail.com>
2024-06-10 07:18:03 +0100

    Pycodestyle error fixed

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Returns a function lookup()"""
 
-def lookup(obj):
-    """Function that returns a list of available attributes and methods of an object
 
+def lookup(obj):
+    """Function that returns a list of available attributes and methods
+    of an object
     Returns:
         list: list of available attributes and methods of an object
     """
+
     return dir(obj)
