fc8e2759788c3f4bd94bb9a92fc89d205edafc92
merytpeters <merytpeters@gmail.com>
2024-06-10 08:25:44 +0100

    Public instance method area that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Empty Class File"""
+
+
+class BaseGeometry:
+    """Empty BaseGeometry Class"""
+
+    def area(self):
+        """Public instance method of the class"""
+
+        raise Exception("area() is not implemented")
