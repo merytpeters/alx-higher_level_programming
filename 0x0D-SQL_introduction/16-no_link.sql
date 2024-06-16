3b642efb9dd3605fa59028b133c706f687b0cd13
merytpeters <merytpeters@gmail.com>
2024-06-16 12:32:55 +0100

    Lists the names and scores of second table in descending order, new data was added

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,4 @@
--- list score and name of table second_table
+-- Add new data and list score and name of table second_table
+INSERT INTO second_table (score, name) VALUES (18, 'Aria');
+INSERT INTO second_table (score, name) VALUES (12, 'Aria');
 SELECT score, name FROM second_table ORDER BY score DESC;
