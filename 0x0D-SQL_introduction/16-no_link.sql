f460dbc3e5945f5a49db0cce95a19032bdb4d2f3
merytpeters <merytpeters@gmail.com>
2024-06-16 12:43:58 +0100

    Lists the names and scores of second table in descending order, new data was added, clause for name being null included

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- Add new data and list score and name of table second_table
 INSERT INTO second_table (score, name) VALUES (18, 'Aria');
 INSERT INTO second_table (score, name) VALUES (12, 'Aria');
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
