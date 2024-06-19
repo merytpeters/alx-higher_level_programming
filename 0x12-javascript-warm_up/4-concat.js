ccdeec82edb00d51ca796144051209bdf205eba4
merytpeters <merytpeters@gmail.com>
2024-06-19 15:04:58 -0700

    prints two arguments passed to it, in the following format: is

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { argv } = process;
+let StrArg = '';
+if (argv.length === 3) {
+  console.log(argv[2], 'is undefined');
+} else if (argv.length >= 4) {
+  for (let i = 3; i < argv.length; i++) {
+    StrArg += argv[i] + ' ';
+  }
+  console.log(argv[2] + ' is ' + StrArg);
+} else {
+  console.log('undefined is undefined');
+}
