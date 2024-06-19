c742a967ae94902ec48a50a4431e5fade8f176e0
merytpeters <merytpeters@gmail.com>
2024-06-19 15:15:37 -0700

    prints two arguments passed to it, in the following format: is, modified a condition

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,7 +3,7 @@ const { argv } = process;
 let StrArg = '';
 if (argv.length === 3) {
   console.log(argv[2], 'is undefined');
-} else if (argv.length >= 4) {
+} else if (argv.length >= 3) {
   for (let i = 3; i < argv.length; i++) {
     StrArg += argv[i] + ' ';
   }
