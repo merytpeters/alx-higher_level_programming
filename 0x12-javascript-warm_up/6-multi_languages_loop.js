2fd57d8cf5fb579f2ef7fed668bca8596ed8cf20
merytpeters <merytpeters@gmail.com>
2024-06-19 16:52:37 -0700

    Script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop, The first line: “C is fun”, The second line: “Python is cool”, The third line: “JavaScript is amazing”, You must use console.log(...) to print all output, You are not allowed to use var, You are not allowed to use any if/else statement, can use only one console.log, must use a loop (while, for, etc.), semistandard error fixed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const multi = 'C is fun\nPython is cool\nJavaScript is amazing';
 const lines = multi.split('\n');
-for (line of lines) {
+for (const line of lines) {
   console.log(line);
 }
