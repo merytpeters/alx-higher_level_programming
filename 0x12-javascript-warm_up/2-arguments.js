7ac0c4c5ce62389914e7709fdf2eb0481d4869d8
merytpeters <merytpeters@gmail.com>
2024-06-19 12:42:55 -0700

    Prints a message depending on number of arguments passed, semistandard errors fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 const { argv } = process;
 let message;
-if (argv.length == 2) {
-	message = 'No argument';
-}
-else if (argv.length  == 3) {
-	message = 'Argument found';
-}
-else if (argv.length >= 4) {
-	message = 'Arguments found';
+if (argv.length === 2) {
+  message = 'No argument';
+} else if (argv.length === 3) {
+  message = 'Argument found';
+} else if (argv.length >= 4) {
+  message = 'Arguments found';
 }
 console.log(message);
