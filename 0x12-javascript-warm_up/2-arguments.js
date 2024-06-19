b5d679df1b4e3e0c53496eabb8cf038bf7762db5
merytpeters <merytpeters@gmail.com>
2024-06-19 12:32:29 -0700

    Prints a message depending on number of arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { argv } = process;
+let message;
+if (argv.length == 2) {
+	message = 'No argument';
+}
+else if (argv.length  == 3) {
+	message = 'Argument found';
+}
+else if (argv.length >= 4) {
+	message = 'Arguments found';
+}
+console.log(message);
