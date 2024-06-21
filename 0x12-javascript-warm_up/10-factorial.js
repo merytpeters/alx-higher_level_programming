e618622d8561522ed0220c1b66961fe8211c2751
merytpeters <merytpeters@gmail.com>
2024-06-21 15:37:11 -0700

    Computes and prints a factorial, factorial of NaN is 1, do it recursively, must use a function, must print out with console.log

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,15 +2,20 @@
 const { argv } = process;
 
 function factorial (n) {
-  console.log(n * factorial(n - 1));
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
+
 if (argv.length !== 3) {
-  console.log(NaN);
+  console.log(1);
 } else {
-  const n = parseInt(process.argv[2], 10);
-  if (isNaN(n) || n <= 0) {
+  const num = parseInt(process.argv[2], 10);
+  if (isNaN(num) || num < 0) {
     console.log(1);
   } else {
-    factorial(n);
+    console.log(factorial(num));
   }
 }
