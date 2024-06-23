a1637dec5e35fc2ffd1f83f9f0ca85a8d3982513
merytpeters <merytpeters@gmail.com>
2024-06-23 15:32:46 -0700

    New state inserted

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Inserts Louisiana"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        # Number of arguments specified
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        database_name = sys.argv[3]
+
+    # Create engine and connect to the MySQL database
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(mysql_username, mysql_password, database_name)
+    )
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # Creates a new state object
+    new_state = State(name="Louisiana")
+
+    #Add the new state to the session
+    session.add(new_state)
+
+    # Commit the session
+    session.commit()
+
+    # Print id of new state
+    print("{}".format(new_state.id))
+
+    # Close session
+    session.close()
