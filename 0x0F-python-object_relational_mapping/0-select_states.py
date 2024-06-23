fb255d5152b79fbe19e1552c014e313aeae33a93
merytpeters <merytpeters@gmail.com>
2024-06-23 07:00:43 -0700

    Python script that lists all states from a database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+
+
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    # Main
+    if len(sys.argv) == 4:
+        # Connection to a MySQL server database
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8"
+        )
+
+        # Object to interact with the database
+        cursor = db.cursor()
+
+        # Execute the query to list all states
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+        # Fetcha all rows returned by the query
+        states = cursor.fetchall()
+
+        # Print the fetched states
+        for state in states:
+            print(state)
+
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
