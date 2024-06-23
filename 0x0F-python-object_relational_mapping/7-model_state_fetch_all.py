39e193fe51312f79311fa8865d5305ba3b3fb936
merytpeters <merytpeters@gmail.com>
2024-06-23 14:22:21 -0700

    Lists all State objects from database, via sqlachemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        # Number of arguments specified
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        database_name = sys.argv[3]
+
+    # Create engine and connect to the MySQL database
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(mysql_username, mysql_password, database_name)
+    )
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # Query all states and sort by id in asc order
+    states = session.query(State).order_by(State.id).all()
+
+    # Print the fetched states
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    # Close session
+    session.close()
