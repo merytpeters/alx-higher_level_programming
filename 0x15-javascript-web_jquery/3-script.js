69d516808ef59965491a16fa0b6aec5b402ea8e0
merytpeters <merytpeters@gmail.com>
2024-08-07 21:46:52 +0100

    Add the class red to div tag red_header

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+(document).ready(function () {
+  // Add a click event listener to div
+  ('#red_header').click(function () {
+    // Select the header element using Jquery
+    ('header').addClass('red');
+  });
+});
