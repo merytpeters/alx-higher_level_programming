eaab529d38f212071884d7e4db1bb4ed2f523763
merytpeters <merytpeters@gmail.com>
2024-08-07 20:44:53 +0100

    No JQuery, update the text color

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Selcet the header element
+  const headerElement = document.querySelector('header');
+
+  // Update the text color of the header element
+  if (headerElement) {
+    headerElement.style.color = '#FF0000';
+  }
+});
