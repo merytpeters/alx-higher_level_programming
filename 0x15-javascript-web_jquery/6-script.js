9d1479eef103a212638d7a06989fb11fa637ec7d
merytpeters <merytpeters@gmail.com>
2024-08-07 22:15:46 +0100

    Update header text

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  // Add a click event listener to div
+  $('#update_header').click(function () {
+    // Update the text of header
+    $('header').text('New Header!!!');
+  });
+});
