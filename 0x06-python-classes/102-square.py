b50c4fd0cc492a3732fb21bd71007800e3f83f62
merytpeters <merytpeters@gmail.com>
2024-08-29 10:59:48 +0100

    Square instance can answer to comparators

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """This class represents a square."""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if (size >= 0):
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if (value >= 0):
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
