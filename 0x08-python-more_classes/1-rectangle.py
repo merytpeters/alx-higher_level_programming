58e448d5a72ae7bebf20f17816e7b092545c14bc
merytpeters <merytpeters@gmail.com>
2024-08-29 11:37:43 +0100

    Class rectangle , instance attribute width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """class Rectangle"""
     def __init__(self, width=0, height=0):
-        self.__height = height
         self.__width = width
+        self.__height = height
 
     @property
     def width(self):
