127421f2bbc36af6d3638912e57409616f7ab8f9
merytpeters <merytpeters@gmail.com>
2024-08-29 11:48:55 +0100

    Class rectangle , instance attribute width and height, correct output required as given

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,11 +13,6 @@ class Rectangle:
         """Gets the width of the rectangle"""
         return self.__width
 
-    @property
-    def height(self):
-        """Gets the height"""
-        return self.__height
-
     @width.setter
     def width(self, value):
         """Sets the width"""
@@ -26,6 +21,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height"""
+        return self.__height
 
     @height.setter
     def height(self, value):
