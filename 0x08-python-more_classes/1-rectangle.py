2730d88870f2cbf6a4bc7395ca3466cd0439d829
merytpeters <merytpeters@gmail.com>
2024-08-29 12:08:47 +0100

    Class rectangle , instance attribute width and height, correct output required as given

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,21 @@
 class Rectangle:
     """class Rectangle"""
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        if isinstance(height, int):
+            if (height >= 0):
+                self.__height = height
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+
+        if isinstance(width, int):
+            if (width >= 0):
+                self.__width = width
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
 
     @property
     def width(self):
@@ -18,7 +31,7 @@ class Rectangle:
         """Sets the width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if (value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -32,6 +45,6 @@ class Rectangle:
         """Gets the height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if (value < 0):
             raise ValueError("height must be >= 0")
         self.__height = value
