9d2a9f87ede9a336a1af1dbecbf4b4f961022745
merytpeters <merytpeters@gmail.com>
2024-08-29 10:38:41 +0100

    position should be use by using space - Don’t fill lines by spaces when position[1] > 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,7 +47,7 @@ class Square:
     def my_print(self):
         if (self.__size == 0):
             print()
-        for i in range(self.__position[1] > 0):
+        for i in range(self.__position[1]):
             print()
         for i in range(self.__size):
             print(" " * self.__position[0] + "#" * self.size)
