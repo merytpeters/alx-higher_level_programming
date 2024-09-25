08cae7c0f429f5308a96b4ab8f0bb5bf2595d359
merytpeters <merytpeters@gmail.com>
2024-09-25 12:32:32 +0100

    Tests add integer function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-"""Addition function"""
+"""Addition function
+Checks for negative and positive infinity float
+"""
 
 
 def add_integer(a, b=98):
-    """Adds two integer"""
+    """Adds two integer
+    Checks that a and b are integers
+    Checks if float is infinity"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    result = int(a) + int(b)
+    if result == float('inf') or result == -float('inf'):
+        raise OverflowError("Result is too large")
+    return result
+    
+
